ac7ca9f7636bfe949dc7215e177c31329079c022
kertku <kertku@ttu.ee>
2022-06-27 21:11:56 +0300

    Implement create event with _ctx!

--- WebApp/Controllers/EventsController.cs
+++ WebApp/Controllers/EventsController.cs
@@ -1,5 +1,7 @@
 using DAL.App.EF;
+using Domain.App;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebApp.Controllers;
@@ -17,4 +19,27 @@ public class EventsController : Controller
     {
         return View(await _ctx.Events.ToListAsync());
     }
+    
+    public async Task<IActionResult> Create()
+    {
+  
+        return  View();
+    }
+
+    // POST: PartInPartsStocks/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Event @event)
+    {
+        if (ModelState.IsValid)
+        {
+            _ctx.Events.Add(@event);
+            await _ctx.SaveChangesAsync();
+            return RedirectToAction( nameof(Index), "Home");
+        }
+        return View(@event);
+    }
+    
 }
