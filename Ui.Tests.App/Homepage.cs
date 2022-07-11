9d8f792e60f003d8c498c3b336c11e0eb0dd2d30
kertku <kertku@ttu.ee>
2022-07-11 05:25:41 +0300

    Clean code.

--- Ui.Tests.App/Homepage.cs
+++
@@ -1,9 +0,0 @@
-﻿using OpenQA.Selenium;
-
-namespace Ui.Tests.App;
-
-public class Homepage
-{
-    private readonly IWebDriver _driver;
-    private const string URI = "https://localhost:7086/";
-}
